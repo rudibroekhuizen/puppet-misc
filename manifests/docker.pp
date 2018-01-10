e04b5d51424f3539a7cbda8f9f8a462b64a0aecc
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-01-10 13:07:48 +0100

    manage_kernel => false
    aws error

--- manifests/docker.pp
+++ manifests/docker.pp
@@ -3,7 +3,8 @@
 class misc::docker {
 
   class {'docker':
-    docker_users => ['rudi.broekhuizen'],
+    docker_users  => ['rudi.broekhuizen'],
+    manage_kernel => false
   }
 
   class {'docker::compose':
